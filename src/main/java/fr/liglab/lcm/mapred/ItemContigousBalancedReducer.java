df94abfe86c581ee65eff4ae903f7834530f1896
Martin Kirchgessner <martin@mkir.ch>
2013-03-20 00:10:51 +0100

    another contigous reducer

--- src/main/java/fr/liglab/lcm/mapred/ItemContigousBalancedReducer.java
+++ src/main/java/fr/liglab/lcm/mapred/ItemContigousBalancedReducer.java
@@ -5,14 +5,15 @@ import java.util.TreeSet;
 
 import fr.liglab.lcm.util.ItemAndBigSupport;
 
-public class ItemContigousReducer extends ItemGroupingReducer {
+public class ItemContigousBalancedReducer extends ItemGroupingReducer {
 	
 	@Override
 	protected void computeGroupsFromFreqHeap(TreeSet<ItemAndBigSupport> heap, long frequentWordsCount, Context context) 
 			throws IOException, InterruptedException {
 		
-		int itemsPerGroup = heap.size() / this.nbGroups;
-		int gItemCount=0,gid=0,rebased=0;
+		long idealSupportSum = frequentWordsCount / this.nbGroups;
+		long currentSupportSum = 0;
+		int gid=0,rebased=0;
 		
 		for (ItemAndBigSupport entry : heap) {
 			keyW.set(entry.item);
@@ -20,10 +21,10 @@ public class ItemContigousReducer extends ItemGroupingReducer {
 			context.write(keyW, valueW);
 			rebased++;
 			
-			gItemCount ++;
-			if (gItemCount == itemsPerGroup) {
+			currentSupportSum += entry.support;
+			if (currentSupportSum >= idealSupportSum) {
 				gid++;
-				gItemCount = 0;
+				currentSupportSum = 0;
 			}
 		}
 	}
