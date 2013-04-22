ff021a865480e960e3d6fee76199202c435c5908
Martin Kirchgessner <martin@mkir.ch>
2013-04-22 18:32:05 +0200

    adding commented notes for future work

--- src/main/java/fr/liglab/lcm/internals/DatasetFactory.java
+++ src/main/java/fr/liglab/lcm/internals/DatasetFactory.java
@@ -1,8 +1,45 @@
 package fr.liglab.lcm.internals;
 
+import java.util.Iterator;
+
+import fr.liglab.lcm.io.FileReader;
+import fr.liglab.lcm.mapred.writables.TransactionWritable;
+
 public final class DatasetFactory {
 	
+	public static Dataset fromFile(int minsup, FileReader reader) {
+		// TODO 
+		// wrap reader in a copier-n-translator-to-TransactionsReader
+		// add TransactionsRebasingDecorator(int[] original)
+	}
 	
+	public static Dataset fromHadoop(int minsup, Iterator<TransactionWritable> input) {
+		// TODO
+		// wrap input in another copier-n-translator
+		// add TransactionsFilteringDecorator(int[] original)
+	}
+	
+	public static Dataset project(Dataset parent, int extension)
+		throws DontExploreThisBranchException {
+		
+		
+		/**
+		 * if (parent instanceof concatenatedDatasetView)
+		 * 	parent.getIgnoredItems
+		 * 
+		 * if (parent.ppTest >= 0)
+		 * 	throw new DontExploreThisBranchException
+		 * 
+		 * counter = new DatasetCounters(parent.getCounters().minSup, parent.getSupport(extension), ignoredItems + extension )
+		 * 
+		 * if (max(counter.closure) > extension))
+		 * 	throw new DontExploreThisBranchException
+		 * 
+		 * 
+		 * then invoke actual dataset's constructor
+		 * 
+		 */
+	}
 	
 	/**
 	 * Thrown when you shouldn't try to work on an extension
