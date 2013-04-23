355ba5e7342e1740da7f7ca7364b2f5ef84dc5bc
Martin Kirchgessner <martin@mkir.ch>
2013-04-23 16:02:23 +0200

    indentation fix

--- src/main/java/fr/liglab/lcm/internals/DatasetFactory.java
+++ src/main/java/fr/liglab/lcm/internals/DatasetFactory.java
@@ -73,19 +73,19 @@ public final class DatasetFactory {
 	 * Thrown when you shouldn't try to work on an extension
 	 */
 	public static class DontExploreThisBranchException extends Exception {
-	        private static final long serialVersionUID = 2969583589161047791L;
-	
-	        public final int firstParent;
-	        public final int extension;
-	
-	        /**
-	         * @param extension the tested extension
-	         * @param foundFirstParent
-	         *            a item found in closure > extension
-	         */
-	        public DontExploreThisBranchException(int exploredExtension, int foundFirstParent) {
-	                this.firstParent = foundFirstParent;
-	                this.extension = exploredExtension;
-	        }
+        private static final long serialVersionUID = 2969583589161047791L;
+
+        public final int firstParent;
+        public final int extension;
+
+        /**
+         * @param extension the tested extension
+         * @param foundFirstParent
+         *            a item found in closure > extension
+         */
+        public DontExploreThisBranchException(int exploredExtension, int foundFirstParent) {
+                this.firstParent = foundFirstParent;
+                this.extension = exploredExtension;
+        }
 	}
 }
