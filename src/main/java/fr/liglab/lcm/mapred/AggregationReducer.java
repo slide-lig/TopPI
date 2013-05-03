1ce18c36db0257cdd3f341a84c782168188c7048
Vincent Leroy <vincent.leroy@imag.fr>
2013-05-03 16:23:04 +0700

    fixed reducer for last phase

--- src/main/java/fr/liglab/lcm/mapred/AggregationReducer.java
+++ src/main/java/fr/liglab/lcm/mapred/AggregationReducer.java
@@ -1,7 +1,5 @@
 package fr.liglab.lcm.mapred;
 
-import java.util.Iterator;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -10,55 +8,45 @@ import fr.liglab.lcm.mapred.writables.ItemAndSupportWritable;
 import fr.liglab.lcm.mapred.writables.SupportAndTransactionWritable;
 import gnu.trove.map.TIntIntMap;
 
-public class AggregationReducer extends Reducer<ItemAndSupportWritable, SupportAndTransactionWritable, IntWritable, SupportAndTransactionWritable> {
-	
+public class AggregationReducer extends
+		Reducer<ItemAndSupportWritable, SupportAndTransactionWritable, IntWritable, SupportAndTransactionWritable> {
+
 	protected final IntWritable keyW = new IntWritable();
 	protected final SupportAndTransactionWritable valueW = new SupportAndTransactionWritable();
-	
+
 	protected TIntIntMap reverseBase = null;
-	
-	protected int k,lastItem,lastCount;
-	
+
+	protected int k;
+
 	@Override
-	protected void setup(Context context) throws java.io.IOException , InterruptedException {
-		
+	protected void setup(Context context) throws java.io.IOException, InterruptedException {
+
 		if (this.reverseBase == null) {
 			Configuration conf = context.getConfiguration();
-			
+
 			this.k = conf.getInt(Driver.KEY_DO_TOP_K, -1);
 			this.reverseBase = DistCache.readReverseRebasing(conf);
 		}
-		
-		this.lastItem = -1;
-		this.lastCount = 0;
 	}
-	
-	protected void reduce(ItemAndSupportWritable key, java.lang.Iterable<SupportAndTransactionWritable> patterns, Context context)
-			throws java.io.IOException, InterruptedException {
-		
-		if (key.getItem() != this.lastItem) {
-			this.lastCount = 0;
-			this.lastItem = key.getItem();
-			
-			int rebased = this.reverseBase.get(this.lastItem);
-			this.keyW.set(rebased);
-		}
-		
-		if (this.lastCount < this.k) {
-			Iterator<SupportAndTransactionWritable> it = patterns.iterator();
-			while (it.hasNext() && this.lastCount < this.k) {
-				SupportAndTransactionWritable entry = it.next();
-				int[] transaction = entry.getTransaction();
-				
-				for (int i = 0; i < transaction.length; i++) {
-					transaction[i] = this.reverseBase.get(transaction[i]);
-				}
-				
-				this.valueW.set(entry.getSupport(), transaction);
-				context.write(this.keyW, this.valueW);
-				this.lastCount++;
+
+	protected void reduce(ItemAndSupportWritable key, java.lang.Iterable<SupportAndTransactionWritable> patterns,
+			Context context) throws java.io.IOException, InterruptedException {
+
+		int rebased = this.reverseBase.get(key.getItem());
+		this.keyW.set(rebased);
+		int count = 0;
+		for (SupportAndTransactionWritable entry : patterns) {
+			int[] transaction = entry.getTransaction();
+			for (int i = 0; i < transaction.length; i++) {
+				transaction[i] = this.reverseBase.get(transaction[i]);
+			}
+			this.valueW.set(entry.getSupport(), transaction);
+			context.write(this.keyW, this.valueW);
+			count++;
+			if (count == this.k) {
+				break;
 			}
 		}
 	}
-	
+
 }
