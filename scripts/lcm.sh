efc06660cf518cf4a1380e0aaae169aae675996e
Martin Kirchgessner <martin@mkir.ch>
2014-10-13 11:18:12 +0200

    removing useless scripts/ folder

--- scripts/lcm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TROVE="$HOME/.m2/repository/net/sf/trove4j/trove4j/3.0.3/trove4j-3.0.3.jar"
-export HADOOP_CLASSPATH="$HOME/Labo/lcm-over-hadoop/target/classes:$TROVE"
-
-hadoop fr.liglab.lcm.Main -libjars $TROVE $* 
